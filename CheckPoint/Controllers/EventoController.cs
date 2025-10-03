d4c43c8fc292347522dd5266399087d844b4b872
Francesco Monteiro Di Benedetto <161745078+FrancescoDiBenedetto@users.noreply.github.com>
2025-10-02 22:26:23 -0300

    inicio do cp

---
+++ CheckPoint/Controllers/EventoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheckPoint.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+    }
+}
---
+++ CheckPoint/Model/Evento.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CheckPoint.Model
+{
+
+    public class Evento
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Titulo")]
+        public required string Titulo { get; set; }
+
+        [BsonElement("Descricao")]
+        public required string descricao { get; set; }
+
+        [BsonElement("Data")]
+        public required DateTime Data { get; set; }
+
+        [BsonElement("Local")]
+        public string? Local { get; set; }
+
+        [BsonElement("Categoria")]
+        public string? Categoria { get; set; }
+
+        [BsonElement("CapacidadeMaxima")]
+        public int? CapacidadeMaxima { get; set; }
+
+        [BsonElement("DataCriacao")]
+        public required DateTime DataCriacao { get; set; }
+
+    }
+}
